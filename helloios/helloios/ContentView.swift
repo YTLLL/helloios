bbc22e83dc52c991d0311a5612296869e1dd1e5f
Yutian Lei <ryanslavs@gmail.com>
2021-04-06 18:27:30 +0800

    test project one

---
+++ helloios/helloios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  helloios
+//
+//  Created by Lei Ryan on 4/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Go Huskie!!!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ helloios/helloios/helloiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloiosApp.swift
+//  helloios
+//
+//  Created by Lei Ryan on 4/6/21.
+//
+
+import SwiftUI
+
+@main
+struct helloiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
